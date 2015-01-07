4bfe39d76c4453b1b9d16a8948339236da7f6389
unknown <thanhlai@outlook.com>
2015-01-07 00:48:26 -0800

    displaying an egg in the main activity

--- MyPetAndroid/src/com/meganet/mypetandroid/MainActivity.java
+++ MyPetAndroid/src/com/meganet/mypetandroid/MainActivity.java
@@ -1,36 +1,70 @@
 package com.meganet.mypetandroid;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.support.v7.app.ActionBarActivity;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	//Constants
+	String eggImg = "egg.png";
+	String loveButtonImg = "heart_red.png";
+	
+	ImageView petImageView, loveButtonImageView;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setup();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private void setup()
+    {
+    	petImageView = (ImageView) findViewById(R.id.petImageViewId);
+    	petImageView.setImageBitmap(getBitmapFromAsset(eggImg));
+    	
+    	loveButtonImageView = (ImageView) findViewById(R.id.loveButtonImageViewId);
+    	loveButtonImageView.setImageBitmap(getBitmapFromAsset(loveButtonImg));
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    // Helper
+    private Bitmap getBitmapFromAsset(String fileName)
+    {
+        AssetManager assetManager = getAssets();
+        InputStream istr = null;
+        try {
+            istr = assetManager.open(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+        Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        return bitmap;
     }
+    
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
